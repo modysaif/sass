44e8312dd2f57bba35712ecdbb501aacf264c889
Mohammed Saif <mohammedsaif630850@gmail.com>
2022-10-13 14:28:59 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let nav = document.querySelector('.nav-list')
+
+let bar = document.querySelector('.nav-bar i')
+
+bar.addEventListener('click' , ()=>{
+
+    nav.classList.toggle('active')
+})
+
+
+console.log(bar)
